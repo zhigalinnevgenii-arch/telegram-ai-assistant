e5a79caaf8c5823c9c28c03af71992b1e07eefec
zhigalinnevgenii-arch <zhigalinnevgenii@gmail.com>
2026-04-17 17:09:26 +0700

    Update index.js

--- index.js
+++ index.js
@@ -63,6 +63,8 @@ app.post("/webhook", async (req, res) => {
   }
 });
 
+const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
 });
